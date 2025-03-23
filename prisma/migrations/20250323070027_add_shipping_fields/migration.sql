bf944d84b815e344fc8cbff536ab6b37ba62ec11
Robherto Matthew <Robhertomatt@gmail.com>
2025-03-23 18:37:10 +0700

    remove Migration File and Folder

--- prisma/migrations/20250323070027_add_shipping_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN     "shippingFee" DOUBLE PRECISION,
-ADD COLUMN     "shippingMethod" TEXT;
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "role" "UserRole" NOT NULL DEFAULT 'CUSTOMER';
